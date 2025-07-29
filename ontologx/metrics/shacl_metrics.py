10d35ead665bb61915084e3cb4fbe84373207920
Luca <luca.cotti@unibs.it>
2025-07-29 11:25:58 +0000

    feat: Working SHACL validation with pySHACL

--- ontologx/metrics/shacl_metrics.py
+++ ontologx/metrics/shacl_metrics.py
@@ -4,31 +4,69 @@ import functools
 
 import pyshacl
 import rdflib
+from owlrl import DeductiveClosure, RDFS_Semantics
 
 from ontologx.store import GraphDocument
+
+
+def _expand_namespace_prefix(name: str, namespaces: dict[str, rdflib.URIRef]) -> rdflib.URIRef:
+    """Expand a namespace prefix in a node type to a full URI."""
+    if ":" not in name:
+        return rdflib.URIRef(name)
+
+    prefix, local_name = name.split(":", 1)
+    if prefix not in namespaces:
+        msg = f"Namespace prefix '{prefix}' not found in the provided namespaces."
+        raise ValueError(msg)
+
+    return rdflib.URIRef(namespaces[prefix].toPython() + local_name)
+
+
+def _convert_to_rdflib(data_graph: GraphDocument, ontology_graph: rdflib.Graph) -> rdflib.Graph:
+    """Convert a GraphDocument to an rdflib Graph."""
+    res = rdflib.Graph()
+    namespaces = dict(ontology_graph.namespace_manager.namespaces())
+    namespaces["olx"] = namespaces.pop("")
+
+    for node in data_graph.nodes:
+        node_uri = rdflib.URIRef(str(node.id))
+        res.add((node_uri, rdflib.RDF.type, _expand_namespace_prefix(node.type, namespaces)))
+
+        for prop, value in node.properties.items():
+            if prop == "uri":
+                continue
+
+            res.add((node_uri, _expand_namespace_prefix(prop, namespaces), rdflib.Literal(value)))
+
+    for rel in data_graph.relationships:
+        start_uri = rdflib.URIRef(str(rel.source.id))
+        end_uri = rdflib.URIRef(str(rel.target.id))
+        res.add((start_uri, _expand_namespace_prefix(rel.type, namespaces), end_uri))
+
+    DeductiveClosure(RDFS_Semantics).expand(res)
+
+    return res
 
 
 class SHACLMetrics:
     """Class to calculate SHACL metrics for a given graph."""
 
     def __init__(self, graph: GraphDocument, ontology_path: str, shacl_path: str) -> None:
-        self.__data_graph = self.__convert_to_rdflib(graph)
         self.__ontology_graph = rdflib.Graph()
         self.__ontology_graph.parse(ontology_path, format="turtle")
 
         self.__shacl_graph = rdflib.Graph()
         self.__shacl_graph.parse(shacl_path, format="turtle")
 
+        self.__data_graph = _convert_to_rdflib(graph, self.__ontology_graph)
+
     @functools.cached_property
     def violations_ratio(self) -> float:
         """Calculate the ratio of SHACL violations to total constraints."""
-        total_violations = self.__total_violations
-        total_constraints = self.__total_constraints
-
-        if total_constraints == 0:
+        if self.__total_constraints == 0:
             return 0.0
 
-        return total_violations / total_constraints
+        return self.__total_violations / self.__total_constraints
 
     @functools.cached_property
     def compliance_ratio(self) -> float:
@@ -43,60 +81,22 @@ class SHACLMetrics:
             float: number of SHACL constraints that are violated
 
         """
-        _, results_graph, _ = pyshacl.validate(
+        _, res_graph, _ = pyshacl.validate(
             self.__data_graph,
             shacl_graph=self.__shacl_graph,
             ont_graph=self.__ontology_graph,
-            inference="rdfs",
+            inference="none",
         )
-        if not isinstance(results_graph, rdflib.Graph):
+
+        if not isinstance(res_graph, rdflib.Graph):
             msg = "Validation results are not in rdflib.Graph format."
             raise TypeError(msg)
 
-        return len(
-            results_graph.query("""
-            SELECT (COUNT(?violation) AS ?count)
-            WHERE {
-                ?violation a <http://www.w3.org/ns/shacl#ValidationResult> .
-            }
-            """),
-        )
+        violations = list(res_graph.subject_predicates(rdflib.URIRef("http://www.w3.org/ns/shacl#ValidationResult")))
+        return len(violations)
 
     @functools.cached_property
     def __total_constraints(self) -> int:
         """Get the total number of SHACL constraints."""
-        query = """
-        SELECT (COUNT(?shape) AS ?count)
-        WHERE {
-            ?shape a <http://www.w3.org/ns/shacl#NodeShape> .
-        }
-        """
-        result = self.__shacl_graph.query(query)
-        return len(result)
-
-    def __convert_to_rdflib(self, graph: GraphDocument) -> rdflib.Graph:
-        """Convert a GraphDocument to an rdflib Graph."""
-        res = rdflib.Graph()
-
-        for node in graph.nodes:
-            node_uri = rdflib.URIRef(str(node.id))
-            res.add((node_uri, rdflib.RDF.type, rdflib.URIRef(node.type)))
-
-            for prop, value in node.properties.items():
-                if prop == "uri" or prop.startswith("schema"):
-                    continue
-
-                res.add((node_uri, rdflib.URIRef(prop), rdflib.Literal(value)))
-
-        for rel in graph.relationships:
-            start_uri = rdflib.URIRef(str(rel.source.id))
-            end_uri = rdflib.URIRef(str(rel.target.id))
-            res.add((start_uri, rdflib.URIRef(rel.type), end_uri))
-
-            for prop, value in rel.properties.items():
-                if prop == "uri" or prop.startswith("schema"):
-                    continue
-
-                res.add((start_uri, rdflib.URIRef(prop), rdflib.Literal(value)))
-
-        return res
+        constraints = list(self.__shacl_graph.subject_objects(rdflib.URIRef("http://www.w3.org/ns/shacl#property")))
+        return len(constraints)
