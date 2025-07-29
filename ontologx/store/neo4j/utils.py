3f10190b917ef11edd0bdb442598b89f29754600
Luca <luca.cotti@unibs.it>
2025-07-29 23:04:30 +0000

    fix: Missing replace in norm out graph

--- ontologx/store/neo4j/utils.py
+++ ontologx/store/neo4j/utils.py
@@ -19,7 +19,7 @@ def normalize_output_graph(graph: GraphDocument) -> GraphDocument:
 
     for node in norm.nodes:
         # Convert namespace separator for node
-        node.type = node.type.replace("__", ":")
+        node.type = node.type.replace("n4sch", "rdfs").replace("__", ":")
 
         # Convert namespace separator for properties, rename "n4sch" to "rdfs",
         for key in list(node.properties.keys()):
@@ -33,7 +33,7 @@ def normalize_output_graph(graph: GraphDocument) -> GraphDocument:
 
     for relationship in norm.relationships:
         # Convert namespace separator for relationship type
-        relationship.type = relationship.type.replace("__", ":")
+        relationship.type = relationship.type.replace("n4sch", "rdfs").replace("__", ":")
 
         # Convert namespace separator for properties, rename "n4sch" to "rdfs"
         for key in list(relationship.properties.keys()):
@@ -58,7 +58,7 @@ def normalize_input_graph(graph: GraphDocument) -> GraphDocument:
 
     for node in norm.nodes:
         # Copy node without properties for normalization
-        node.type = node.type.replace(":", "__")
+        node.type = node.type.replace("rdfs", "n4sch").replace(":", "__")
 
         # Insert uri property
         node.properties["uri"] = node.id
@@ -70,7 +70,7 @@ def normalize_input_graph(graph: GraphDocument) -> GraphDocument:
 
     for relationship in norm.relationships:
         # Convert namespace separator for relationship type
-        relationship.type = relationship.type.replace(":", "__")
+        relationship.type = relationship.type.replace("rdfs", "n4sch").replace(":", "__")
 
         # Convert namespace separator for properties, rename "rdfs" to "n4sch"
         for key in list(relationship.properties.keys()):
